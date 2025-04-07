bc7c616d2f42e1916c7507aa736261b48e6327ca
ana <ana.alston@student.onslow.school.nz>
2025-04-08 10:40:39 +1200

    Started work on menu system and added the main tree.

--- formative-assessment/Program.cs
+++ formative-assessment/Program.cs
@@ -1,16 +1,30 @@
 ﻿// I came up with the name _after_ I created the solution
-namespace birdsorter;
+
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace birdkeeper;
 
 /// <summary>
-/// Interface for any of the items -or nodes- in the tree   
+/// Interface for any of the nodes in the tree   
 /// </summary>
 interface INode
 {
     string Name { get; set; }
+    [JsonIgnore]
     TaxonomicGrouping? Parent { get; set; }
 
     // Dummy function to avoid errors as both classes will execute this differently
-    public INode? Search(string query) { return null; }
+    public List<INode> Search(string query) { return new List<INode>(); }
+    
+    public string GetPath()
+    {
+        if (Parent != null)
+        {
+            return Parent.GetPath() + "/" + Name;
+        }
+        return Name;
+    }
 }
 
 /// <summary>
@@ -24,8 +38,20 @@ class TaxonomicGrouping: INode
     }
     
     public string Name { get; set; }
+    [JsonIgnore]
     public TaxonomicGrouping? Parent { get; set; }
-    public List<INode> Children { get; set; } = new List<INode>();
+    public List<INode> Children { get; set; } = new();
+
+    /// <summary>
+    /// Makes the current grouping print its children out.
+    /// </summary>
+    public void GetChildren()
+    {
+        foreach (INode child in Children)
+        {
+            Console.WriteLine(child.Name);
+        }
+    }
 
     /// <summary>
     /// Adds a child to this node's tree under this node
@@ -59,32 +85,24 @@ class TaxonomicGrouping: INode
     /// </summary>
     /// <param name="query">Name to search for</param>
     /// <returns>A list of possible matches</returns>
-    public List<INode>? Search(string query)
+    public List<INode> Search(string query)
     {
-        if (query == Name)
+        List<INode> matches = new List<INode>();
+
+        if (Name == query)
         {
-            return new List<INode> { this };
+            matches.Add(this);
         }
 
         if (Children.Count > 0)
         {
-            List<INode> matches = new List<INode>();
             foreach (INode child in Children)
             {
-                INode? result = child.Search(query);
-                if (result != null)
-                {
-                    matches.Add(result);
-                }
-            }
-
-            if (matches.Count > 0)
-            {
-                return matches;
+                matches.AddRange(child.Search(query));
             }
         }
         
-        return null;
+        return matches;
     }
 
     public override string ToString()
@@ -93,10 +111,143 @@ class TaxonomicGrouping: INode
     }
 }
 
+/// <summary>
+/// Class for any kind of bird
+/// </summary>
+class Bird : INode
+{
+    public Bird(string name, string scientificName)
+    {
+        Name = name;
+        ScientificName = scientificName;
+    }
+    
+    public string Name { get; set; }
+    public string ScientificName { get; set; }
+    [JsonIgnore]
+    public TaxonomicGrouping? Parent { get; set; }
+    
+    /// <summary>
+    /// Gets the placement of the node in its tree
+    /// </summary>
+    /// <returns>A path to this node</returns>
+    public string GetPath()
+    {
+        if (Parent != null)
+        {
+            return Parent.GetPath() + "/" + ScientificName;
+        }
+        return ScientificName;
+    }
+    
+    /// <summary>
+    /// Searches for a node with the name given.
+    /// </summary>
+    /// <param name="query">Name to search for</param>
+    /// <returns>A list of possible matches</returns>
+    public List<INode> Search(string query)
+    {
+        List<INode> matches = new List<INode>();
+
+        if (Name == query || ScientificName == query)
+        {
+            matches.Add(this);
+        }
+        
+        return matches;
+    }
+
+    public override string ToString()
+    {
+        return ScientificName + " - " + Name;
+    }
+}
+
 class Program
 {
     static void Main(string[] args)
     {
+        TaxonomicGrouping animalia = new TaxonomicGrouping("Animalia");
+        TaxonomicGrouping chordata = new TaxonomicGrouping("Chordata");
+        TaxonomicGrouping aves = new TaxonomicGrouping("Aves");
+        animalia.AddChild(chordata);
+        chordata.AddChild(aves);
         
+        TaxonomicGrouping psittiaciformes = new TaxonomicGrouping("Psittitaciformes");
+        TaxonomicGrouping apterygiformes = new TaxonomicGrouping("Apterygiformes");
+        TaxonomicGrouping passeriformes = new TaxonomicGrouping("Passeriformes");
+        aves.AddChild(psittiaciformes);
+        aves.AddChild(apterygiformes);
+        aves.AddChild(passeriformes);
+        
+        TaxonomicGrouping strigopidae = new TaxonomicGrouping("Strigopidae");
+        TaxonomicGrouping apterygidae = new TaxonomicGrouping("Apterygidae");
+        TaxonomicGrouping rhipidurae = new TaxonomicGrouping("Rhipidurae");
+        TaxonomicGrouping meliphagidae = new TaxonomicGrouping("Meliphagidae");
+        psittiaciformes.AddChild(strigopidae);
+        apterygiformes.AddChild(apterygidae);
+        passeriformes.AddChild(rhipidurae);
+        passeriformes.AddChild(meliphagidae);
+        
+        TaxonomicGrouping nestor = new TaxonomicGrouping("Nestor");
+        TaxonomicGrouping apteryx = new TaxonomicGrouping("Apteryx");
+        TaxonomicGrouping rhipidura = new TaxonomicGrouping("Rhipidura");
+        TaxonomicGrouping prosthemadera = new TaxonomicGrouping("Prosthemadera");
+        strigopidae.AddChild(nestor);
+        apterygidae.AddChild(apteryx);
+        rhipidurae.AddChild(rhipidura);
+        meliphagidae.AddChild(prosthemadera);
+
+        Bird kaka = new Bird("Kaka", "Meridionalis");
+        Bird kea = new Bird("Kea", "Notabilis");
+        nestor.AddChild(kaka);
+        nestor.AddChild(kea);
+
+        Bird kiwi = new Bird("Little Spotted Kiwi", "Owenii");
+        apteryx.AddChild(kiwi);
+
+        Bird piwakawaka = new Bird("Piwakawaka", "Fuliginosa");
+        rhipidura.AddChild(piwakawaka);
+
+        Bird tui = new Bird("Tui", "Novaeseelandiae");
+        prosthemadera.AddChild(tui);
+        
+        while (false)
+        {
+            Console.WriteLine("1. Add new bird\n2. Add new grouping\n3. Search\n4. Exit");
+            Console.Write("Enter choice: ");
+            string choice = Console.ReadLine();
+            if (choice == "1")
+            {
+                bool validGrouping = false;
+                while (validGrouping)
+                {
+                    Console.Write("Enter grouping: ");
+                    string grouping = Console.ReadLine();
+                    List<INode> results = animalia.Search(grouping);
+                    if (results.Count == 0)
+                    {
+                        Console.WriteLine("Invalid grouping, try again!");
+                    }
+                    else if (results.Count > 1)
+                    {
+                        Console.WriteLine("Please pick which grouping you want to add to.");
+                        int index = 0;
+                        foreach (INode node in results)
+                        {
+                            index += 1;
+                            Console.WriteLine(node.GetPath());
+                        }
+                    }
+            }
+                
+                Console.Write("Enter name: ");
+                string name = Console.ReadLine();
+                Console.Write("Enter scientific name: ");
+                string scientificName = Console.ReadLine();
+            }
+        }
+        Console.WriteLine(JsonSerializer.Serialize(aves));
+        Console.WriteLine(JsonSerializer.Serialize(animalia));
     }
 }
